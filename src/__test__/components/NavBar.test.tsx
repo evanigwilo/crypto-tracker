551f9f4019102e504554f765d069091ad5d8a82b
Evan Igwilo <uvclassic@gmail.com>
2022-03-06 17:55:15 +0000

    Finishing Navbar Component and Test todos

--- src/__test__/components/NavBar.test.tsx
+++ src/__test__/components/NavBar.test.tsx
@@ -1,26 +1,67 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, render, screen, fireEvent } from '@testing-library/react';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import ThemeContext from '../../context/themeContext';
+import NavBar from "../../components/NavBar";
 
 describe("AppBar", () => {
     beforeEach(() => {
+        render(<NavBar />);
     });
     afterEach(() => {
         cleanup();
     });
 
-    it.todo("should contain title");
-    it.todo("should contain theme toggle");
-    it.todo("should contain Github icon");
-    it.todo("should contain LinkedIn icon");
+    it("should contain title", () => {
+        const title = screen.getByText(/Cryptocurrency Price Tracker/i);
+        expect(title).toBeVisible();
+    });
+    it("should contain theme toggle", () => {
+        const darkIcon = screen.queryByTestId(/Brightness7Icon/i);
+        const lightIcon = screen.queryByTestId(/Brightness4Icon/i);
+        expect(darkIcon || lightIcon).toBeVisible();
+    });
+    it("should contain Github icon", () => {
+        const label = screen.getByLabelText(/Github/i);
+        expect(label).toBeVisible();
+
+    });
+    it("should contain LinkedIn icon", () => {
+        const label = screen.getByLabelText(/LinkedIn/i);
+        expect(label).toBeVisible();
+    });
 });
 
 describe("AppBar Functionality", () => {
 
     beforeEach(() => {
+        const queryClient = new QueryClient();
+        render(
+            <ThemeContext>
+                <QueryClientProvider client={queryClient}>
+                    <NavBar />
+                </QueryClientProvider>
+            </ThemeContext>
+        );
     });
     afterEach(() => {
         cleanup();
     });
 
-    it.todo("should toggle the right theme");
-
+    it("should toggle the correct theme", () => {
+        let darkIcon = screen.queryByTestId(/Brightness7Icon/i);
+        let lightIcon = screen.queryByTestId(/Brightness4Icon/i);
+        if (lightIcon) {
+            fireEvent.click(lightIcon.parentElement!);
+            darkIcon = screen.queryByTestId(/Brightness7Icon/i);
+            expect(darkIcon).toBeVisible();
+            expect(lightIcon).not.toBeVisible();
+        } else if (darkIcon) {
+            fireEvent.click(darkIcon.parentElement!);
+            lightIcon = screen.queryByTestId(/Brightness4Icon/i);
+            expect(lightIcon).toBeVisible();
+            expect(darkIcon).not.toBeVisible();
+        }
+    });
 });
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,62 @@
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Tooltip from '@mui/material/Tooltip';
+import { useTheme } from '@mui/material/styles';
+
+import { useThemeState } from '../context/themeContext';
+
 export default function NavBar() {
-    return <div>NavBar</div>
+    const theme = useTheme();
+    const { toggleColorMode } = useThemeState();
+
+    return (
+        <AppBar
+            data-testid='NavBar'
+            position="sticky"
+            elevation={2}
+            enableColorOnDark
+            color='default'
+            sx={{ backgroundColor: 'transparent', padding: '0px 5px' }}>
+
+            <Toolbar disableGutters variant='dense' sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    sx={{ width: '100%', position: 'absolute', justifyContent: 'space-around' }}
+                >
+                    Cryptocurrency Price Tracker
+                </Typography>
+
+                <Tooltip title={`Toggle ${theme.palette.mode === 'dark' ? 'Light' : 'Dark'} Mode`}>
+                    <IconButton sx={{ color: theme.palette.text.secondary }} onClick={() => toggleColorMode()} color="inherit">
+                        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                    </IconButton>
+                </Tooltip>
+
+                <div>
+                    <Tooltip title={`GitHub`}>
+                        <IconButton href="https://github.com/evanigwilo" target="_blank" rel="noopener noreferrer"
+                            sx={{ color: theme.palette.text.secondary }}
+                            color="inherit">
+                            <GitHubIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={`LinkedIn`}>
+                        <IconButton href="https://www.linkedin.com/in/evanigwilo" target="_blank" rel="noopener noreferrer"
+                            sx={{ color: theme.palette.text.secondary }}
+                            color="inherit">
+                            <LinkedInIcon />
+                        </IconButton>
+                    </Tooltip>
+                </div >
+            </Toolbar>
+
+        </AppBar>
+    );
 }
