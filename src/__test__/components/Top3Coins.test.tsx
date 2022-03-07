904aae65582f6320cd5f51531b3ef743e0e4f874
Evan Igwilo <uvclassic@gmail.com>
2022-03-07 06:30:13 +0000

    Added Top3Coins Component and Test todos

---
+++ src/__test__/components/Top3Coins.test.tsx
@@ -0,0 +1,16 @@
+import { cleanup } from '@testing-library/react';
+
+describe("Top coins by market rank", () => {
+    beforeEach(() => {
+    });
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    it.todo("should show left coin");
+
+    it.todo("should show center coin");
+
+    it.todo("should show right coin");
+});
